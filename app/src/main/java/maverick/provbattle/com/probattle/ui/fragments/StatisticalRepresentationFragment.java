e00f97a168ea94e9b6d6b9e0f641ae5dc0102a85
Roshaanf <roshaan.farrukh@yahoo.com>
2018-03-31 16:45:10 +0500

    graph

--- app/src/main/java/maverick/provbattle/com/probattle/ui/fragments/StatisticalRepresentationFragment.java
+++ app/src/main/java/maverick/provbattle/com/probattle/ui/fragments/StatisticalRepresentationFragment.java
@@ -7,7 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -18,6 +20,8 @@ import java.util.List;
 
 import maverick.provbattle.com.probattle.R;
 import maverick.provbattle.com.probattle.databinding.FragmentStatisticalRepresentationBinding;
+import maverick.provbattle.com.probattle.ui.SharedPreferenceManager;
+import maverick.provbattle.com.probattle.ui.model.CryptoModel;
 
 
 /**
@@ -31,13 +35,15 @@ public class StatisticalRepresentationFragment extends Fragment implements View.
     private OnFragmentInteractionListener mListener;
     FragmentStatisticalRepresentationBinding binding;
     List<String> change;
-
+    String spinnerValueChange="",spinnerValueNames;
+    List<CryptoModel> list;
+    List<String> cryptoNames;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(LayoutInflater.from(getContext()), R.layout.fragment_statistical_representation, container, false);
-        change= new ArrayList<>();
+        change = new ArrayList<>();
         change.add("1 hour change");
         change.add("24 hours change");
         change.add("7 days change");
@@ -46,22 +52,133 @@ public class StatisticalRepresentationFragment extends Fragment implements View.
                 android.R.layout.simple_spinner_item,
                 change
         );
+        list=SharedPreferenceManager.getDetails(getActivity());
+        for(CryptoModel model: list){
+            cryptoNames.add(model.getName());
+        }
+        ArrayAdapter<String> nameAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_spinner_item,
+                cryptoNames
+        );
         binding.changeSpinner.setAdapter(adapter);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
+        binding.graph.setVisibility(View.INVISIBLE);
+
+        binding.changeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                spinnerValueChange=change.get(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //Another interface callback
+            }
         });
 
-        binding.graph.addSeries(series);
+        binding.changeSpinnerCryptoName.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                spinnerValueNames=cryptoNames.get(position);
+                setValueOnGraph();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //Another interface callback
+            }
+        });
+
+//        binding.graph.addSeries(series);
         setupListener();
         return binding.getRoot();
     }
 
 
     private void setupListener() {
+    }
+
+    public void setValueOnGraph(){
+        double percentChange;
+
+        CryptoModel value=null;
+        for(CryptoModel model: list){
+            if(spinnerValueNames.equals(model.getName())){
+                value=model;
+                break;
+            }
+        }
+
+        if(value==null){
+            return;
+        }
+
+        if(spinnerValueChange.equals("1 hour change")){
+            double sum=Double.parseDouble(value.getPrice_usd());
+            percentChange=Double.parseDouble(value.getPercent_change_1h());
+            double value1,value2,value3,value4;
+                sum+=percentChange*Double.parseDouble(value.getPrice_usd());
+                value1=sum;
+                sum+=percentChange*Double.parseDouble(value.getPrice_usd());
+                value2=sum;
+                sum+=percentChange*Double.parseDouble(value.getPrice_usd());
+                value3=sum;
+                sum+=percentChange*Double.parseDouble(value.getPrice_usd());
+                value4=sum;
+                    LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
+                new DataPoint(0, value1),
+                new DataPoint(1, value2),
+                new DataPoint(2, value3),
+                new DataPoint(3, value4),
+        });
+            binding.graph.setVisibility(View.VISIBLE);
+            binding.graph.addSeries(series);
+        }
+        else if(spinnerValueChange.equals("24 hours change")){
+            double sum=Double.parseDouble(value.getPrice_usd());
+            percentChange=Double.parseDouble(value.getPercent_change_1h());
+            double value1,value2,value3,value4;
+            sum+=percentChange*Double.parseDouble(value.getPrice_usd());
+            value1=sum;
+            sum+=percentChange*Double.parseDouble(value.getPrice_usd());
+            value2=sum;
+            sum+=percentChange*Double.parseDouble(value.getPrice_usd());
+            value3=sum;
+            sum+=percentChange*Double.parseDouble(value.getPrice_usd());
+            value4=sum;
+            LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
+                    new DataPoint(0, value1),
+                    new DataPoint(1, value2),
+                    new DataPoint(2, value3),
+                    new DataPoint(3, value4),
+            });
+            binding.graph.setVisibility(View.VISIBLE);
+            binding.graph.addSeries(series);
+        }
+        else if(spinnerValueChange.equals("7 days change")){
+            double sum=Double.parseDouble(value.getPrice_usd());
+            percentChange=Double.parseDouble(value.getPercent_change_1h());
+            double value1,value2,value3,value4;
+            sum+=percentChange*Double.parseDouble(value.getPrice_usd());
+            value1=sum;
+            sum+=percentChange*Double.parseDouble(value.getPrice_usd());
+            value2=sum;
+            sum+=percentChange*Double.parseDouble(value.getPrice_usd());
+            value3=sum;
+            sum+=percentChange*Double.parseDouble(value.getPrice_usd());
+            value4=sum;
+            LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
+                    new DataPoint(0, value1),
+                    new DataPoint(1, value2),
+                    new DataPoint(2, value3),
+                    new DataPoint(3, value4),
+            });
+            binding.graph.setVisibility(View.VISIBLE);
+            binding.graph.addSeries(series);
+        }
+        else{
+            Toast.makeText(getActivity(),"Please select change.",Toast.LENGTH_SHORT).show();
+        }
 
     }
 
